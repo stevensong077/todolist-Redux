012bb5b69859d7ab9faf636b51ff379ecb8dc093
steven <steven@CXY-HappyHackerss-iMac.local>
2020-11-02 16:40:40 +1100

    add useEffect and saga

--- src/Todolist2.js
+++ src/Todolist2.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "antd/dist/antd.css";
 import { Input, List, Checkbox } from "antd";
 import todoActions from "./store/actions";
 import { connect } from "react-redux";
-
+import { useState, useEffect } from "react"
 const Todolist2 = props => {
-  const { inputChange, pressEnter, toggle,inputValue, list } = props;
+  const { inputChange, pressEnter, toggle, inputValue, list } = props;
   const handleInputChange = e => {
     const value = e.target.value;
     console.log(value);
@@ -17,37 +17,56 @@ const Todolist2 = props => {
   const handleToggle = (e, index) => {
     toggle(index)
   };
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-      <div>
-        <Input
-          value={inputValue}
-          placeholder="todo info"
-          style={{ width: "300px", marginRight: "3px" }}
-          onChange={handleInputChange}
-          onPressEnter={handlePressEnter}
+    <Fragment>
+
+
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+        <div>
+          <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+              Click me
+    </button>
+          </div>
+          <Input
+            value={inputValue}
+            placeholder="todo info"
+            style={{ width: "300px", marginRight: "3px" }}
+            onChange={handleInputChange}
+            onPressEnter={handlePressEnter}
+          />
+        </div>
+        <List
+          style={{ marginRight: "30px" }}
+          bordered
+          // dataSource={list} // get data from reducer directly
+          dataSource={list}
+          renderItem={(item, index) => (
+            <List.Item
+              style={{
+                marginTop: "10px",
+                textDecoration: item.isCompleted ? "line-through" : "none"
+              }}
+            >
+              <Checkbox onChange={value => handleToggle(value, index)}>
+                {" "}
+              </Checkbox>
+              {item.text}
+            </List.Item>
+          )}
         />
       </div>
-      <List
-        style={{ marginRight: "30px" }}
-        bordered
-        // dataSource={list} // get data from reducer directly
-        dataSource={list}
-        renderItem={(item, index) => (
-          <List.Item
-            style={{
-              marginTop: "10px",
-              textDecoration: item.isCompleted ? "line-through" : "none"
-            }}
-          >
-            <Checkbox onChange={value => handleToggle(value, index)}>
-              {" "}
-            </Checkbox>
-            {item.text}
-          </List.Item>
-        )}
-      />
-    </div>
+    </Fragment>
+
   );
 };
 // export default Todolist2;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
-import { createStore } from 'redux';
+import { createStore,applyMiddleware,compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer'
+import { helloSaga } from './saga';
 
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+  applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(helloSaga)
 
 export default store; 
